cd262291459338df54ca7fa2aa45f55f1ede92c7
James Hughff <jh5624@ic.ac.uk>
2024-12-21 15:21:43 +0000

    Changed json files location

--- item_type.py
+++ item_type.py
@@ -15,9 +15,14 @@ class ItemType:
         self.accounts = accounts
 
     def get_info(self, name="default"):
-        self.translation_data = tools.load_json_file("user/translation.json", name)
-        self.upload_data = tools.load_json_file("user/upload.json", name)
-        self.download_data = tools.load_json_file("user/download.json")
+        try:
+            self.translation_data = tools.load_json_file("user/translation.json", name)
+            self.upload_data = tools.load_json_file("user/upload.json", name)
+            self.download_data = tools.load_json_file("user/download.json")
+        except:
+            self.translation_data = tools.load_json_file("_internal/user/translation.json", name)
+            self.upload_data = tools.load_json_file("_internal/user/upload.json", name)
+            self.download_data = tools.load_json_file("_internal/user/download.json")
 
         if hasattr(self, "accounts"):
             self.accounts.update_title()
